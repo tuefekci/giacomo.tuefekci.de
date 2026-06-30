de5e7f94955018592c73cf8662cd0b4f7e2719b5
Giacomo Tüfekci <giacomo@tuefekci.de>
2026-06-30 15:06:45 +0200

    fixing data based issue.

--- docs/_app/immutable/chunks/stores.82c0f48e.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.73018ef6.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
