5578fdf41f1f0cfa7db0a31538e20ba3081b28a3
Giacomo Tüfekci <giacomo@tuefekci.de>
2026-06-30 16:02:11 +0200

    updated the build.

--- docs/_app/immutable/chunks/stores.20e90e42.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.eede6e52.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
